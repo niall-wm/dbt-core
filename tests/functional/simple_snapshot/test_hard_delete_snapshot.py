bf0ceba3305985007fcd089a1b7a7389463daf83
Peter Allen Webb <peter.webb@dbtlabs.com>
2022-11-29 13:06:25 -0500

    CT-1405: Restore unit test which was only failing locally

--- tests/functional/simple_snapshot/test_hard_delete_snapshot.py
+++ tests/functional/simple_snapshot/test_hard_delete_snapshot.py
@@ -60,7 +60,6 @@ def macros():
     return {"test_no_overlaps.sql": macros__test_no_overlaps_sql}
 
 
-@pytest.mark.skip()  # how on earth did this break :|
 def test_snapshot_hard_delete(project):
     # run the first snapshot
     datetime_snapshot()
